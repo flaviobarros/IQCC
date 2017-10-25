3f919daa09dd2144ad74bafea2efc65919877ba5
flavio <flaviomargarito@gmail.com>
2017-10-25 18:19:23 -0200

    Corrigindo problemas com o check do CRAN.

--- R/stats.R
+++ R/stats.R
@@ -48,21 +48,21 @@ stats <- function(datum, m, n, p)
         q <- array(dim = c(p, p, m))
         w <- array(dim = c(n, p, m))
         M2 <- array(dim = c(n, p, m))
-        media <- colMeans(datum)                    # média de cada face do array, deve ser uma matriz com 20 linhas e 2 colunas
-        m1 <- matrix(media, m, p, byrow = T)        # matriz com as 20 médias
-        mm <- colMeans(m1)                          # média das médias
+        media <- colMeans(datum)                    # mean of each face of the array, must be a matrix with 20 rows and 2 columns
+        m1 <- matrix(media, m, p, byrow = T)        # matrix with the 20 means
+        mm <- colMeans(m1)                          # mean of means
         for(i in 1:m)
         {
-            M1 <- matrix(m1[i, ], n, p, byrow = T)  # repetindo cada média da matriz m1 para construir o array de médias e subtrair dos dados
-            M2[, , i] <- M1                         # guardando as repetições em um array
+            M1 <- matrix(m1[i, ], n, p, byrow = T)  # repeating each mean of matrix m1 to build an array of means and subtract from the data
+            M2[, , i] <- M1                         # keep the repetitions in an array
         }
-        w <- datum - M2                             # array com a diferença de X - Xbarra
+        w <- datum - M2                             # array with X - Xbarra
         for(i in 1:m)
         {
-            S <- (t(w[, , i]) %*% w[, , i]) / (n-1) # matriz S
+            S <- (t(w[, , i]) %*% w[, , i]) / (n-1) # matrix S
             q[, , i] <- S
         }
-        mS <- rowMeans(q, dims = 2)                 # média da matriz de var-cov
+        mS <- rowMeans(q, dims = 2)                 # mean of the matrix of the var-cov
         return(list(mm, mS, m1))
     }
 }
