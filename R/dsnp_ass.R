8b3426861a7b3668415e4a29fbffefd2a32ad7f3
Flavio Barros <flaviomargarito@gmail.com>
2026-07-12 13:42:39 -0300

    refactor: reuse unified DS-np performance core for ASS

--- R/dsnp_ass.R
+++ R/dsnp_ass.R
@@ -1,54 +1,33 @@
 #' Double-Sampling np Chart: Average Sample Size
 #'
-#' Compute the Average Sample Size (ASS) for the double-sampling np chart
-#' at a given nonconforming proportion p.
+#' Compute the Average Sample Size for the double-sampling np chart.
 #'
-#' The ASS is the expected total number of items inspected per signal
-#' decision. If the process is accepted or signals at the first stage,
-#' n1 items are inspected. If the second stage is required, n1 + n2 items
-#' are inspected. ASS = n1 + n2 * P(second stage is required).
-#'
-#' @param p The nonconforming proportion to evaluate. Can be a scalar or
-#' numeric vector, with values in [0, 1].
-#' @param n1 The first-stage sample size (positive integer).
-#' @param n2 The second-stage sample size (positive integer).
-#' @param wl The fractional warning limit. Must be less than ucl1.
-#' @param ucl1 The fractional upper control limit for the first stage.
-#' @return A list with the following elements:
-#' \describe{
-#'   \item{ass}{The Average Sample Size: n1 + n2 * p_second.}
-#'   \item{p_second}{Probability of requiring the second sample.}
-#'   \item{n1, n2, wl, ucl1}{The input chart parameters.}
-#' }
+#' @param p Nonconforming proportion, scalar or vector in [0, 1].
+#' @param n1 First-stage sample size.
+#' @param n2 Second-stage sample size.
+#' @param wl Fractional warning limit.
+#' @param ucl1 Fractional first-stage upper control limit.
+#' @return A list containing ASS, second-stage probability, and chart
+#' parameters.
 #' @export
-#' @author Daniela R. Recchia, Emanuel P. Barbosa
 #' @seealso \link{dsnp_prob_accept}, \link{dsnp_arl}
-#' @references Joekes, S., Smrekar, M. and Barbosa, E. (2015). Extending a
-#' double sampling control chart for non-conforming proportion in high quality
-#' processes to the case of small samples.
-#' @importFrom stats pbinom
 #' @examples
-#'
-#' # Published example from Joekes et al. (2015)
-#' res <- dsnp_ass(0.005, n1 = 34, n2 = 162,
-#'                 wl = 1.5, ucl1 = 2.5)
-#' res$ass  # approximately 35.94
+#' dsnp_ass(0.005, 34, 162, 1.5, 2.5)$ass
 #'
 dsnp_ass <- function(p, n1, n2, wl, ucl1)
 {
-    if(any(p < 0 | p > 1))
-        stop("p must be between 0 and 1")
-    if(n1 < 1 || n1 != floor(n1))
-        stop("n1 must be a positive integer")
-    if(n2 < 1 || n2 != floor(n2))
-        stop("n2 must be a positive integer")
-    if(wl >= ucl1)
-        stop("wl must be less than ucl1")
+    p <- .dsnp_validate_probability(p)
+    n1 <- .dsnp_validate_size(n1, "n1")
+    n2 <- .dsnp_validate_size(n2, "n2")
+    th <- .dsnp_thresholds(wl, ucl1)
 
-    wl_accept   <- floor(wl)
-    ucl1_reject <- floor(ucl1) + 1
+    p_second <- if(th$d1_lower > th$d1_upper)
+        rep(0, length(p))
+    else
+        stats::pbinom(th$d1_upper, n1, p) -
+            stats::pbinom(th$wl_accept, n1, p)
 
-    p_second <- pbinom(ucl1_reject - 1, n1, p) - pbinom(wl_accept, n1, p)
+    p_second <- pmin(1, pmax(0, p_second))
     ass <- n1 + n2 * p_second
 
     list(
@@ -56,7 +35,7 @@ dsnp_ass <- function(p, n1, n2, wl, ucl1)
         p_second = p_second,
         n1 = n1,
         n2 = n2,
-        wl = wl,
-        ucl1 = ucl1
+        wl = th$wl,
+        ucl1 = th$ucl1
     )
 }
